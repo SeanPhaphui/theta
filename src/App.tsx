649a2e1a2f09919503bb63705f46da1273f37f35
Sean Phaphui <seanphaphui@gmail.com>
2023-05-21 00:37:28 -0700

    add basic date progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { MobileDatePicker } from '@mui/x-date-pickers';
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
+import { CircularProgress } from '@mui/material';
 
 function App() {
+  const [start, setStart] = useState<Dayjs | null>(null);
+  const [exp, setExp] = useState<Dayjs | null>(null);
+  const [current, setCurrent] = useState<number>(0);
+  const [days, setDays] = useState<number | null>(null);
+
+  const currentDate = dayjs();
+
+  useEffect(() => {
+    calculateDaysBetweenDates();
+  }, [start, exp]);
+
+  const calculateDaysBetweenDates = () => {
+    if (start && exp) {
+      const totalDays = exp.diff(start, 'day');
+      const daysPassed = currentDate.diff(start, 'day');
+      const percentage = (daysPassed / totalDays) * 100;
+      setCurrent(Math.round(percentage));
+      setDays(totalDays);
+    }
+  };
+
+  const firstDateHandler = (newValue: Dayjs | null) => {
+    setStart(newValue);
+  };
+
+  const secondDateHandler = (newValue: Dayjs | null) => {
+    setExp(newValue);
+  };
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <div className="App">
@@ -15,15 +45,13 @@ function App() {
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
-          <MobileDatePicker defaultValue={dayjs('2022-04-17')} />
+          <MobileDatePicker value={start} onChange={firstDateHandler} />
+          <MobileDatePicker value={exp} onChange={secondDateHandler} />
+          {days !== null && <div>{current + '%'}</div>}
+          {days !== null && <CircularProgress variant="determinate" value={current} />}
         </header>
       </div>
     </LocalizationProvider>
