fd3c70f38dcb2f9281d51c55762a914f29d83522
Sean Phaphui <seanphaphui@gmail.com>
2023-05-21 23:44:10 -0700

    add option picker component

---
+++ src/Contract/OptionPicker/OptionPicker.tsx
@@ -0,0 +1,40 @@
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import React, { useState } from "react";
+import "./OptionPicker.css";
+
+interface OptionPickerProps {
+    onOptionChange: (option: string) => void;
+}
+
+const OptionPicker: React.FC<OptionPickerProps> = (props) => {
+    const { onOptionChange } = props;
+    const [alignment, setAlignment] = useState<string>();
+    const handleChange = (
+        event: React.MouseEvent<HTMLElement>,
+        newAlignment: string
+    ) => {
+        setAlignment(newAlignment);
+        onOptionChange(newAlignment);
+    };
+
+    return (
+        <div className="OptionPicker">
+            <ToggleButtonGroup
+                color="primary"
+                fullWidth
+                value={alignment}
+                exclusive
+                onChange={handleChange}
+            >
+                <ToggleButton className="left-toggle-button" value="call">
+                    Call
+                </ToggleButton>
+                <ToggleButton className="right-toggle-button" value="put">
+                    Put
+                </ToggleButton>
+            </ToggleButtonGroup>
+        </div>
+    );
+};
+
+export default OptionPicker;
